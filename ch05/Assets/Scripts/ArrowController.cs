a2575e1f1e091df3679bc40ddaf463876729ceac
Gyutae123 <kkssaln@naver.com>
2026-04-01 15:44:00 +0900

    고양이 이동 구현

---
+++ ch05/Assets/Scripts/ArrowController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    public float dropSpeed = 0.1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -dropSpeed, 0);
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
