87730c936dc314caa7671c5fbea008aadeec5622
Gyutae123 <kkssaln@naver.com>
2026-04-13 11:01:10 +0900

    고양이게임 완성

--- ch05/Assets/Scripts/ArrowController.cs
+++ ch05/Assets/Scripts/ArrowController.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
+
+    GameObject player;
+    GameObject manager;
+    float minDistance = 1.1f;
     public float dropSpeed = 0.1f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    // Start is called once beforpe the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.Find("player");
+        manager = GameObject.Find("GameManager");
     }
 
     // Update is called once per frame
@@ -14,9 +21,18 @@ public class ArrowController : MonoBehaviour
     {
         transform.Translate(0, -dropSpeed, 0);
 
-        if(transform.position.y < -10)
+        if(transform.position.y < -7)
         {
             Destroy(gameObject);
+        }
+        Vector2 p1 = transform.position;
+        Vector2 p2 = player.transform.position;
+        float distance = (p1 - p2).magnitude;
+        if(distance < minDistance)
+        {
+            manager.GetComponent<GameManager>().DecreaseHP();
+            Destroy(gameObject);
+
         }
     }
 }
