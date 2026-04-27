1a07b1f88b6fbe78a87380cce55ad8ccc6ac1857
Gyutae123 <kkssaln@naver.com>
2026-04-27 10:32:16 +0900

    고양이 골

--- ch06/Assets/Scripts/Player.cs
+++ ch06/Assets/Scripts/Player.cs
@@ -7,40 +7,68 @@ public class Player : MonoBehaviour
     public float maxWalkSpeed = 2f;
 
     public Sprite[] walkSprites;
+    public Sprite jumpSprite;
+
     public float animationPeriod = 0.1f;
     float time = 0f;
     int idx = 0;
-    SpriteRenderer sr;
+    Animator animator;
 
+    SpriteRenderer sr;
     Rigidbody2D rb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         Application.targetFrameRate = 60;
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             rb.AddForce(transform.up * jumpForce);
+            rb.AddForce(transform.right * walkForce);
         }
         if(rb.linearVelocityX < maxWalkSpeed)
         {
-            rb.AddForce(transform.right * walkForce);
+            //rb.AddForce(transform.right * walkForce);
 
         }
 
         time += Time.deltaTime;
+        if (rb.linearVelocityY != 0)
+        {
+            animator.SetBool("isJump", true);
+        }
         if (time > animationPeriod)
         {
-            time = 0f;
-            sr.sprite = walkSprites[idx];
-            idx = 1 - idx;
+            animator.SetBool("isJump", false);
+
         }
 
+        //time += Time.deltaTime;
+        //if(rb.linearVelocityY != 0)
+        //{
+        //    sr.sprite = jumpSprite;
+        //}
+        //if (time > animationPeriod)
+        //{
+        //    time = 0f;
+        //    sr.sprite = walkSprites[idx];
+        //    idx++;
+        //    if(idx == walkSprites.Length)
+        //    {
+        //        idx=0;
+        //    }
+        //}
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("goal");
     }
 }
